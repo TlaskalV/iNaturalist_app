d6f22644e4f2c57be681449977c1b37a4be7a175
Vojczech <tlaskal@biomed.cas.cz>
2019-06-30 23:13:08 +0200

    basic plotting
    heatmap and circles added, works for username

--- server.R
+++ server.R
@@ -2,35 +2,30 @@ library(shiny)
 library(rinat)
 library(dplyr)
 library(htmlwidgets)
+library(leaflet.extras)
 
 server <- function(input, output) {
   
   obs_data <- reactive(
-    get_inat_obs_user(username = input$inat_query, maxresults = 30) %>% 
-      filter(!is.na(latitude)) %>% 
-      filter(quality_grade == "research")
+    rinat::get_inat_obs_user(username = input$inat_query, maxresults = 30) %>% 
+      dplyr::filter(!is.na(latitude)) %>% 
+      dplyr::filter(quality_grade == "research")
   )
   
   
   output$map <- renderLeaflet({
+    obs_data <- obs_data()
     leaflet(data = obs_data()) %>%
-      addProviderTiles("CartoDB.Positron", options = tileOptions(minZoom = 7, maxZoom = 13)) %>%
+      addProviderTiles("CartoDB.Positron") %>% 
+      addCircles(~longitude, ~latitude, popup=paste(obs_data$observed_on, "|", obs_data$scientific_name, "|", paste0("<a target='", "_blank' ", "href='", obs_data$url,"'>", "observation","</a>")), weight = 1, radius = 3, color = "black", stroke = TRUE, fillOpacity = 0.6) %>% 
+      addHeatmap(lng = ~longitude, lat = ~latitude, radius = 8, gradient = NULL) %>% 
       addFullscreenControl
   })
-  
-  observe({
-    withProgress(message = 'Plotting heatmap',
-                 detail = 'This may take a while...',
-                 value = 0, {
-                   for (i in 1:15) {
-                     incProgress(1/15)
-                   }
-  leafletProxy("map", data = obs_data()) %>%
-    addHeatmap(lng = ~longitude, lat = ~latitude, radius = 8, gradient = NULL)
-                 })
+
+  output$header <- renderTable({ 
+    obs_data()
   })
-    
-    
+  
 }
 
 # data <- get_inat_obs(taxon_name = "Cannabis sativa", maxresults = 3000000, quality = "research") %>% filter(!is.na(latitude))
