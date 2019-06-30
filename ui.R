cf22c2afbed48acb6c4a6b309573e7d5422c37b8
Vojczech <tlaskal@biomed.cas.cz>
2019-06-30 23:13:38 +0200

    rendering plot, theme fix

--- ui.R
+++ ui.R
@@ -12,23 +12,24 @@ library(shinythemes)
 library(leaflet)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-    # Application title
+navbarPage("iNaturalist heatmap",
+           theme = shinytheme("journal"),
     tabPanel("Map",
              div(class = "outer",
-                 leafletOutput("map", width = "100%", height = "100%"), #
+                 leafletOutput(outputId = "map"),
                  absolutePanel(id = "controls", class = "panel panel-default", 
                                fixed = TRUE,
                                draggable = TRUE, top = "10%", left = "auto", right = 20, bottom = "auto",
                                width = 330, height = "auto", cursor = "move",
                                br(),
                                p("Search and plot iNaturalist observations"),
-                               textInput("inat_query", label = h3("iNat query"), value = "Enter latin name/username"),
+                               textInput("inat_query", label = h3("iNat query"), placeholder = "Enter latin name/username"),
                                br(),
                                p(a("Source code", href = "https://github.com/Vojczech", target = "_blank")),
                                p("Data sources:", a("iNaturalist", 
                                                     href = "https://www.inaturalist.org/", 
-                                                    target = "_blank"), 
+                                                    target = "_blank"),
+                                 "accessed through", 
                                  a("rinat R package", 
                                    href = "https://github.com/ropensci/rinat", 
                                    target = "_blank"), 
@@ -37,7 +38,9 @@ ui <- fluidPage(
                                    href = "https://ropensci.org/", 
                                    target = "_blank")
                                  )
-             )
+             ),
+             br(),
+             tableOutput("header")
              )
     ),
     
