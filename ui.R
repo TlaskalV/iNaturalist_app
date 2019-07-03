94c3fd7444e8b1c6de963fd96c88295ac68ba74c
Vojczech <tlaskal@biomed.cas.cz>
2019-07-03 09:30:20 +0200

    input options change of order

--- ui.R
+++ ui.R
@@ -12,16 +12,23 @@ navbarPage("iNaturalist heatmap",
                  absolutePanel(id = "controls", class = "panel panel-default", 
                                fixed = TRUE,
                                draggable = TRUE, top = "10%", left = "auto", right = "1%", bottom = "auto",
-                               width = 300, height = "auto", cursor = "move", style = "opacity: 0.80; padding: 5px;", 
-                               br(),
+                               width = 300, height = "auto", cursor = "move", style = "opacity: 0.80; padding: 5px;",
                                uiOutput("choose_query"),
                                uiOutput("type_query"),
                                p("Limit number of the newest observations"),
-                               numericInput("num_obs", label = "", value = 100, min = 1, max = 2000000),
-                               p(a(list(icon("github"), "Source code"), href = "https://github.com/Vojczech/", target="_blank")),
-                               p("Data sources:", a("iNaturalist", 
-                                                    href = "https://www.inaturalist.org/", 
-                                                    target = "_blank"),
+                               numericInput("num_obs", label = "", value = 200, min = 1, max = 2000000),
+                               downloadButton("download_map", 
+                                              "Download map"),
+                               br(),
+                               br(),
+                               downloadButton("download_data", 
+                                              "Download data table"),
+                               br(),
+                               br(),
+                               p(a(list(icon("github"), "Source code"), href = "https://github.com/Vojczech/iNaturalist_app", target="_blank")),
+                               p("Data from", a("iNaturalist", 
+                                                href = "https://www.inaturalist.org/", 
+                                                target = "_blank"),
                                  "accessed through", 
                                  a("rinat package", 
                                    href = "https://github.com/ropensci/rinat", 
@@ -30,12 +37,7 @@ navbarPage("iNaturalist heatmap",
                                  a("rOpenSci", 
                                    href = "https://ropensci.org/", 
                                    target = "_blank")
-                                 ),
-                               downloadButton("download_map", 
-                                              "Download map"),
-                               hr(),
-                               downloadButton("download_data", 
-                                              "Download data table")
+                               )
              )
              )
     ),
@@ -44,7 +46,7 @@ navbarPage("iNaturalist heatmap",
              fluidRow(
                  column(12,
                         wellPanel(
-                            includeMarkdown("README.md"))
+                            includeMarkdown("about.md"))
                  )
              )
     )
